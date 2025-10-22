4db7953e8424651fbf2e077e0d1a4f4e1403a634
Adam Essenmacher <adam.essenmacher@gmail.com>
2025-10-22 16:41:33 -0400

    Ensure MainPage can be constructed by Shell

--- src/MainPage.xaml.cs
+++ src/MainPage.xaml.cs
@@ -1,12 +1,29 @@
 using FloofLog.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FloofLog;
 
 public partial class MainPage : ContentPage
 {
+    public MainPage()
+        : this(GetRequiredService<MainPageViewModel>())
+    {
+    }
+
     public MainPage(MainPageViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    private static TService GetRequiredService<TService>()
+        where TService : notnull
+    {
+        if (Application.Current?.Handler?.MauiContext?.Services is IServiceProvider services)
+        {
+            return services.GetRequiredService<TService>();
+        }
+
+        throw new InvalidOperationException("Unable to resolve service.");
     }
 }
