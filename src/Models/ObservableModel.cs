7a902eabf39d0f02c5292b08a43197d4d15662fb
Adam Essenmacher <adam.essenmacher@gmail.com>
2025-10-22 16:16:09 -0400

    Add pet log domain models and persistence service

---
+++ src/Models/ObservableModel.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FloofLog.Models;
+
+public abstract class ObservableModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(storage, value))
+        {
+            return false;
+        }
+
+        storage = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        if (propertyName is null)
+        {
+            return;
+        }
+
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ src/Models/PetActivity.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace FloofLog.Models;
+
+public sealed class PetActivity : ObservableModel
+{
+    private Guid _id = Guid.NewGuid();
+    private Guid _petId;
+    private string _displayName = string.Empty;
+    private string? _notes;
+    private DateTimeOffset _occurredAt = DateTimeOffset.UtcNow;
+    private DateTimeOffset _createdAt = DateTimeOffset.UtcNow;
+    private DateTimeOffset? _updatedAt;
+    private RecurrenceInfo? _recurrence;
+
+    public Guid Id
+    {
+        get => _id;
+        set => SetProperty(ref _id, value);
+    }
+
+    public Guid PetId
+    {
+        get => _petId;
+        set => SetProperty(ref _petId, value);
+    }
+
+    public string DisplayName
+    {
+        get => _displayName;
+        set => SetProperty(ref _displayName, value);
+    }
+
+    public string? Notes
+    {
+        get => _notes;
+        set => SetProperty(ref _notes, value);
+    }
+
+    public DateTimeOffset OccurredAt
+    {
+        get => _occurredAt;
+        set => SetProperty(ref _occurredAt, value);
+    }
+
+    public DateTimeOffset CreatedAt
+    {
+        get => _createdAt;
+        set => SetProperty(ref _createdAt, value);
+    }
+
+    public DateTimeOffset? UpdatedAt
+    {
+        get => _updatedAt;
+        set => SetProperty(ref _updatedAt, value);
+    }
+
+    public RecurrenceInfo? Recurrence
+    {
+        get => _recurrence;
+        set => SetProperty(ref _recurrence, value);
+    }
+}
